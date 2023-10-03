7dbcda7ec601619bd2b9a5bd7d7ffb7e81dcb479
AlexKennethMiles <apuchkov17@gmail.com>
2023-10-03 23:47:52 +0300

    1.1. Add DB config files

---
+++ db/scripts/002_dml_insert_files.sql
@@ -0,0 +1,7 @@
+insert into files (name, path) values
+('kung_fu_panda_poster.png', './files/kung_fu_panda_poster.png'),
+('life_of_pi_poster.jpg', './files/life_of_pi_poster.jpg'),
+('akira_poster.jpg', './files/akira_poster.jpg'),
+('snatch_poster.jpg', './files/snatch_poster.jpg'),
+('kill_bill_poster.jpg', './files/kill_bill_poster.jpg')
+;
