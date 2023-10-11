4b1f42390b3cc0bf6099416c8dfde079289d84c2
AlexKennethMiles <apuchkov17@gmail.com>
2023-10-11 23:40:53 +0300

    1.4 Edit posters extensions

--- db/scripts/002_dml_insert_files.sql
+++ db/scripts/002_dml_insert_files.sql
@@ -1,7 +1,7 @@
 insert into files (name, path) values
 ('kung_fu_panda_poster.png', './files/kung_fu_panda_poster.png'),
-('life_of_pi_poster.jpg', './files/life_of_pi_poster.jpg'),
-('akira_poster.jpg', './files/akira_poster.jpg'),
-('snatch_poster.jpg', './files/snatch_poster.jpg'),
-('kill_bill_poster.jpg', './files/kill_bill_poster.jpg')
+('life_of_pi_poster.png', './files/life_of_pi_poster.png'),
+('akira_poster.png', './files/akira_poster.png'),
+('snatch_poster.png', './files/snatch_poster.png'),
+('kill_bill_poster.png', './files/kill_bill_poster.png')
 ;
